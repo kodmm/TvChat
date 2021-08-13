471191d2d453aaace51a4416ba7f9f849f0f5001
kodmm <53547701+kodmm@users.noreply.github.com>
2021-08-13 19:53:07 +0900

    feature googleSignin function

---
+++ lib/services/auth.dart
@@ -0,0 +1,44 @@
+
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthMethods {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  getCurrentUser() async {
+    return auth.currentUser;
+  }
+
+
+  signInWithGoogle(BuildContext context) async {
+    final GoogleSignInAccount? googleSignInAccount =
+      await GoogleSignIn().signIn();
+
+    if (googleSignInAccount != null) {
+      final GoogleSignInAuthentication googleAuth =
+        await googleSignInAccount.authentication;
+
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        idToken: googleAuth.idToken,
+        accessToken: googleAuth.accessToken
+      );
+
+      UserCredential result = await auth.signInWithCredential(credential);
+
+      User? userDetails = result.user;
+
+
+
+
+    }
+
+  }
+
+
+
+
+
+}
