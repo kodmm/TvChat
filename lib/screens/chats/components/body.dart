bbd8d6ccf5babf203385f88c1a2f0a832b824c13
kodmm <53547701+kodmm@users.noreply.github.com>
2021-08-15 18:09:56 +0900

    compleate login function

--- lib/screens/chats/components/body.dart
+++ lib/screens/chats/components/body.dart
@@ -7,7 +7,6 @@ import 'chat_card.dart';
 
 class Body extends StatelessWidget {
 
-
   @override
   Widget build(BuildContext context) {
     return Column(
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -2,14 +2,19 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 // import 'package:tvchat/helperfunctions/sharedpref_helper.dart';
 
 class DatabaseMethods {
+  final String broadcastingOffices = "broadcastingOffices";
+  final String users = "users";
+
   Future addUserInfoToDB(
       String userId, Map<String, String> userInfoMap
       ) async {
     return FirebaseFirestore.instance
-        .collection("users")
+        .collection(users)
         .doc(userId)
         .set(userInfoMap);
   }
+
+
 
 
 }
