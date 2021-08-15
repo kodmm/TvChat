e2c122c4c9b6f2a1d3ca8109634ad3722dbdcff0
kodmm <53547701+kodmm@users.noreply.github.com>
2021-08-15 11:21:21 +0900

    create function to addUserInfoToFirestore

---
+++ lib/services/database.dart
@@ -0,0 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:tvchat/helperfunctions/sharedpref_helper.dart';
+
+class DatabaseMethods {
+  Future addUserInfoToDB(
+      String userId, Map<String, String> userInfoMap
+      ) async {
+    return FirebaseFirestore.instance
+        .collection("users")
+        .doc(userId)
+        .set(userInfoMap);
+  }
+
+
+}
